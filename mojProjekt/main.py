53ce363d6be62e3eb6a99ab7adfdefaed8cafd13
Nop3k <robertmorozowski@gmail.com>
2020-06-30 13:37:42 +0200

    wizualizacja - działa i poprawiony wygląd
    dodane komentarze

--- mojProjekt/main.py
+++ mojProjekt/main.py
@@ -13,7 +13,7 @@ def automatic_update():
     f = urlopen(link)
     stock_data = f.read().decode("utf-8")
 
-    # przetworz dane z bossa
+    # przetwórz dane z bossa
     stock_data_listed = [x.split(',') for x in stock_data.split('\r\n')]
     stock_data_listed.pop()
     for stock in stock_data_listed:
@@ -35,6 +35,7 @@ def automatic_update():
 
 # glowna petla
 if __name__ == '__main__':
+    # połącz z bazą danych i otwórz kolekcję
     my_client = pymongo.MongoClient("mongodb://localhost:27017/")
     my_db = my_client["myDatabase"]
     collection = my_db["notowania"]
@@ -55,7 +56,9 @@ if __name__ == '__main__':
             file_no = input('Wybierz plik do zaimportowania do bazy danych:\n')
             file_path = f'{Path().absolute()}\{file_list[int(file_no)]}'
 
-            # Przetworz wybrany plik .prn
+            # Przetworz wybrany plik .prn stringi do intów/floatów
+            # kolejno 0='nazwa', 1='data', 2='otwarcie', 3='max', 4='min',
+            #         5='tko', 6='wolumen'
             with open(file_path, 'r') as file:
                 file_data = file.read().split('\n')
             file_data.pop()
@@ -78,6 +81,7 @@ if __name__ == '__main__':
                 {key: value for key, value in zip(keys, l)} for l
                 in file_data_listed]
 
+            # upewnij się, że dane są nowe
             if collection.find_one({"data": date}):
                 print(f"Baza zawiera dane z dnia {date}.")
             else:
@@ -86,33 +90,54 @@ if __name__ == '__main__':
                     f"Dodano {len(result.inserted_ids)} notowan z dnia {date}")
 
         if choice == '2':
+            # pobierz z bazy dane i utwórz dataframe
             result_data_frame = pd.DataFrame(
                 collection.find({'nazwa': 'CDPROJEKT'}))
+
+            # konwertuj datę z int do datetime
             result_data_frame['data'] = pd.to_datetime(
                 result_data_frame['data'].astype(int), format='%Y%m%d')
+
+            # uporządkuj wyniki zgodnie z datą
             result_data_frame.sort_values(by=['data'], inplace=True)
             print(result_data_frame)
 
+            # przygotuj okno z wykresami
             fig, ax1 = plt.subplots()
-            ax1.plot(result_data_frame['data'], result_data_frame['otwarcie'])
-            ax1.set_xlabel('Data')
-            ax1.set_ylabel('PLN')
-            ax1.autoscale()
+            plt.xticks(rotation=45)
+            plt.autoscale()
+            plt.tight_layout(4)
+            plt.title('CDPROJEKT')
 
+            # przygotuj wykres data/wolumen
+            ax1.bar(result_data_frame['data'], result_data_frame['wolumen'],
+                    color='c')
+            ax1.set_ylabel('Il. transakcji', fontsize=10)
+            ax1.set_xlabel('Data', fontsize=10)
+            ax1.tick_params(axis='x', labelsize=8)
+            ax1.legend(loc="upper left", labels=['wolumen'], fontsize=6)
+
+            # skopiuj dane z wykresu 1 i przygotuj wykres data/otwarcie
             ax2 = ax1.twinx()
-            result_data_frame.plot(kind='bar', x='data', y='wolumen', ax=ax2)
-            ax2.set_ylabel('Il. transakcji')
-            ax2.autoscale()
+            ax2.plot(result_data_frame['data'], result_data_frame['otwarcie'],
+                     '-o')
+            ax2.set_ylabel('PLN', fontsize=10)
+            ax2.legend(loc="upper right", labels=['otwarcie'], fontsize=6)
 
+            # wyświetl wykres
             plt.show()
 
         if choice == '3':
+            # pobierz dane ze strony WWW
             date, data = automatic_update()
+
+            # sprawdź, czy baza posiada "nowe" dane
             if collection.find_one({"data": date}):
                 print(f"Baza zawiera dane z dnia {date}.")
             else:
                 result = collection.insert_many(data)
                 print(
                     f"Dodano {len(result.inserted_ids)} notowan z dnia {date}")
+
         if choice == '0':
             break
