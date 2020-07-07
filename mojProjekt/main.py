01d402f667fe7af15f8f923de31102631ba113ec
Nop3k <robertmorozowski@gmail.com>
2020-07-07 18:37:09 +0200

    dodanie możliwości wybrania okresu do rysowania notowań porządek w kodzie wrapper urlopen

--- mojProjekt/main.py
+++ mojProjekt/main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pymongo
 import pprint
+import datetime
 
 keys = ['nazwa', 'data', 'otwarcie', 'max', 'min', 'tko', 'wolumen']
 
@@ -14,27 +15,37 @@ keys = ['nazwa', 'data', 'otwarcie', 'max', 'min', 'tko', 'wolumen']
 def automatic_update():
     # pobierz najnowsze dane z bossa
     link = "https://info.bossa.pl/pub/ciagle/mstock/sesjacgl/sesjacgl.prn"
-    f = urlopen(link)
-    stock_data = f.read().decode("utf-8")
+    try:
+        response = urlopen(link)
+    except URLError as e:
+        if hasattr(e, 'reason'):
+            print('We failed to reach a server.')
+            print('Reason: ', e.reason)
+        elif hasattr(e, 'code'):
+            print('The server couldn\'t fulfill the request.')
+            print('Error code: ', e.code)
+    else:
+        stock_data = response.read().decode("utf-8")
 
-    # przetwórz dane z bossa
-    stock_data_listed = [x.split(',') for x in stock_data.split('\r\n')]
-    stock_data_listed.pop()
-    for stock in stock_data_listed:
-        stock[1] = int(stock[1])
-        stock[2] = float(stock[2])
-        stock[3] = float(stock[3])
-        stock[4] = float(stock[4])
-        stock[5] = float(stock[5])
-        stock[6] = float(stock[6])
-    date = stock_data_listed[0][1]
-    print(f"BOS SA dane z {date}")
+        # przetwórz dane z bossa
+        stock_data_listed = [x.split(',') for x in stock_data.split('\r\n')]
+        stock_data_listed.pop()
+        for stock in stock_data_listed:
+            stock[1] = int(stock[1])
+            stock[2] = float(stock[2])
+            stock[3] = float(stock[3])
+            stock[4] = float(stock[4])
+            stock[5] = float(stock[5])
+            stock[6] = float(stock[6])
+        date = stock_data_listed[0][1]
+        print(f"BOS SA dane z {date}")
 
-    # przygotuj dokumenty
-    stock_data_documented = [{key: value for key, value in zip(keys, row)} for
-                             row
-                             in stock_data_listed]
-    return int(date), stock_data_documented
+        # przygotuj dokumenty
+        stock_data_documented = [{key: value for key, value in zip(keys, row)}
+                                 for
+                                 row
+                                 in stock_data_listed]
+        return int(date), stock_data_documented
 
 
 # glowna petla
@@ -44,11 +55,11 @@ if __name__ == '__main__':
     my_db = my_client["myDatabase"]
     collection = my_db["notowania"]
     while True:
-        choice = input(
-            '================\n' + '1 update z .prn\n2 plot\n3 update' +
-            '\n0 wyjscie\n================\n')
+        menu = input(
+            '================\n1 update z .prn\n2 update z neta\n3 plot\n' +
+            '0 wyjscie\n================\n')
 
-        if choice == '1':
+        if menu == '1':
             # Pokaz gdzie jestem
             print('Sciezka: ' + str(Path().absolute()))
 
@@ -91,24 +102,56 @@ if __name__ == '__main__':
                         f"Dodano {len(result.inserted_ids)} notowan"
                         f"z dnia {date}")
 
-        if choice == '2':
+        if menu == '2':
+            # pobierz dane ze strony WWW
+            date, data = automatic_update()
+
+            # TODO: funkcja sprawdzająca bazę na obecność danych
+            # sprawdź, czy baza posiada "nowe" dane
+            if collection.find_one({"data": date}):
+                print(f"Baza zawiera dane z dnia {date}.")
+            else:
+                result = collection.insert_many(data)
+                print(
+                    f"Dodano {len(result.inserted_ids)} notowan z dnia {date}")
+
+        if menu == '3':
             # TODO: funkcja pobierająca dane z bazy
             # wybierz spółkę
             company = input('Wybierz spółkę do wyświetlenia\n').upper()
+            query = {}
 
             # pomóż wybrać spółkę po nazwie
-            result_uniques = collection.distinct('nazwa')
-            inquiry = get_close_matches(company, result_uniques)
+            unique_companies = collection.distinct('nazwa')
+            inquiry = get_close_matches(company, unique_companies)
             if not inquiry:
                 print(f'Nie znaleziono spółki {company}.')
-            elif company not in result_uniques:
+            elif company not in unique_companies:
                 print(f"Czy chodziło o:")
                 print({key: value for key, value in enumerate(inquiry)})
-                company = inquiry[int(input())]
+                query['nazwa'] = inquiry[int(input())]
+            else:
+                query['nazwa'] = company
+
+            today = datetime.date.today()
+            choice = input(
+                'Wybierz okres:\n1 ostatnie 7 dni\n2 ostatnie 30 dni\n3 wlasny\n0 wszystko\n')
+            if choice == '1':
+                week_ago = (today - datetime.timedelta(days=8)).strftime(
+                    '%Y%m%d')
+                query['data'] = {'$gte': int(week_ago)}
+            if choice == '2':
+                month_ago = (today - datetime.timedelta(days=31)).strftime(
+                    '%Y%m%d')
+                query['data'] = {'$gte': int(month_ago)}
+            if choice == '3':
+                # TODO: własny okres notowań
+                continue
+            print(query)
 
             # pobierz z bazy dane i utwórz dataframe
             result_data_frame = pd.DataFrame(
-                collection.find({'nazwa': company}))
+                collection.find(query))
 
             # sprawdź, czy poprawnie wyszukano
             if result_data_frame.empty:
@@ -128,7 +171,7 @@ if __name__ == '__main__':
                 plt.xticks(rotation=45)
                 plt.autoscale()
                 plt.tight_layout(4)
-                plt.title(company)
+                plt.title(query['nazwa'])
 
                 # przygotuj wykres data/wolumen
                 ax1.bar(result_data_frame['data'],
@@ -152,18 +195,10 @@ if __name__ == '__main__':
                 # wyświetl wykres
                 plt.show()
 
-        if choice == '3':
-            # pobierz dane ze strony WWW
-            date, data = automatic_update()
+        if menu == '4':
+            # TODO: funkcja scrapująca dane 'realtime' i rysująca
+            pass
 
-            # TODO: funkcja sprawdzająca bazę na obecność danych
-            # sprawdź, czy baza posiada "nowe" dane
-            if collection.find_one({"data": date}):
-                print(f"Baza zawiera dane z dnia {date}.")
-            else:
-                result = collection.insert_many(data)
-            print(
-                f"Dodano {len(result.inserted_ids)} notowan z dnia {date}")
-
-        if choice == '0':
+        # TODO: funkcja porówniania dwóch spółek?
+        if menu == '0':
             break
